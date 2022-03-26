c19bc23cce374399579759a44d34a3441214e737
JacoboOrozco <57960290+JacoboOrozco@users.noreply.github.com>
2022-03-26 11:56:50 -0500

    MultipleFace
    Multiple face recognition in images

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Choose an image to detect faces in
-img = cv2.imread('RDJ.png')
+img = cv2.imread('FRIENDS.png')
 
 #Must convert to grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -13,7 +13,9 @@ grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
 #Draw rectangles around the faces
-cv2.rectangle(img, (220, 143), (220+219, 143+219), (0, 255, 0), 2)
+#cv2.rectangle(img, (220, 143), (220+219, 143+219), (0, 255, 0), 2)
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 print(face_coordinates)
 
