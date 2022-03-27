a3af69d9d1578a0617a0a363f6d1284fbf59d228
JacoboOrozco <57960290+JacoboOrozco@users.noreply.github.com>
2022-03-27 11:06:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,26 +33,8 @@ while True:
     if key == 81 or key == 113:
         break
 
-print("Code Completed")
-
-"""
-
-#Must convert to grayscale
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-#To detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
-#Draw rectangles around the faces
-#cv2.rectangle(img, (220, 143), (220+219, 143+219), (0, 255, 0), 2)
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#To show img
-cv2.imshow('Clever Programmer Face Detector', img)
-
-#In order to pause the execution of the code to be able to se img
-cv2.waitKey()
+#Release the videocapture object
+webcam.release()
 
 print("Code Completed")
-"""
+
