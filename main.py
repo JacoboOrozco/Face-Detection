42b52b5afe117ebcc47fb88d39416fd00ba8c227
JacoboOrozco <57960290+JacoboOrozco@users.noreply.github.com>
2022-04-19 20:33:15 -0500

    Video&Webcam
    No facerecon

--- main.py
+++ main.py
@@ -4,23 +4,54 @@ from PIL import Image, ImageTk
 import cv2
 import imutils
  
+def visualizarJugadores():
+    global cap_jugadores
+    if cap_jugadores is not None:
+        ret, frame = cap_jugadores.read() 
+        if ret == True:
+            frame = imutils.resize(frame, width=640)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            im = Image.fromarray(frame)
+            img = ImageTk.PhotoImage(image=im)
+
+            lbl_jugadores.configure(image=img)
+            lbl_jugadores.image = img
+            lbl_jugadores.after(10, visualizarJugadores)
+        else: 
+            lbl_jugadores.image = ""
+            cap_jugadores.release()   
+
+def iniciarJugadores():
+    global cap_jugadores
+    cap_jugadores = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    visualizarJugadores()
 
 def visualizar():
     global cap
-    ret, frame = cap.read() 
-    if ret == True:
-        frame = imutils.resize(frame, width=720)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    if cap is not None:
+        ret, frame = cap.read() 
+        if ret == True:
+            frame = imutils.resize(frame, width=640)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        im = Image.fromarray(frame)
-        img = ImageTk.PhotoImage(image=im)
+            im = Image.fromarray(frame)
+            img = ImageTk.PhotoImage(image=im)
 
-        lbl_video.configure(image=img)
-        lbl_video.image = img
-        lbl_video.after(10, visualizar)
+            lbl_video.configure(image=img)
+            lbl_video.image = img
+            lbl_video.after(10, visualizar)
+        else: 
+            lbl_info_video_path.configure(text="Aun no se ha seleccionado un video")
+            lbl_video.image = ""
+            cap.release()
 
 def elegirVideo():
     global cap
+    if cap is not None:
+        lbl_video.image = ""
+        cap.release()
+        cap = None
     video_path = filedialog.askopenfilename(filetypes= [
         ("all video format", ".mp4"),
         ("all video format", ".avi")])
@@ -34,6 +65,7 @@ def elegirVideo():
 
 
 cap = None
+cap_jugadores = None
 
 root = Tk()
 
@@ -49,14 +81,20 @@ lbl_info_video_path.grid(column=1, row=1)
 
 
 lbl_video = Label(root)
-lbl_video.grid(column=0, row=2, columnspan=2)
+lbl_video.grid(column=0, row=2, columnspan=3)
 
-"""
+
+btn_iniciar_jugadores = Button(root, text = "Iniciar", width=45, command=iniciarJugadores)
+btn_iniciar_jugadores.grid(column=0, row=3, padx=5, pady=5)
+
+btn_finalizar_jugadores = Button(root, text = "Finalizar", width=45)
+btn_finalizar_jugadores.grid(column=1, row=3, padx=5, pady=5)
+
 lbl_info_jugadores = Label(root, text = "A continuacion se muestran los jugadores")
-lbl_info_jugadores.grid(column=0, row=3)
+lbl_info_jugadores.grid(column=0, row=4)
 
 lbl_jugadores = Label(root)
-lbl_jugadores.grid(column=0, row=4)
-"""
+lbl_jugadores.grid(column=0, row=5, columnspan=3)
+
 
 root.mainloop()
